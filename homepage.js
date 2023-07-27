ddfd7ffc9a3da6d17d40b77813d52c6f82df9fd4
Pokepoison <jordyaw7@gmail.com>
2023-07-27 01:12:42 -0400

    adds dex info

--- homepage.js
+++ homepage.js
@@ -1,36 +1,36 @@
-var testHeading = document.getElementById('test');
+// var testHeading = document.getElementById('test');
 
-var apiUrl = 'https://pokeapi.co/api/v2/type/3';
+// var apiUrl = 'https://pokeapi.co/api/v2/type/3';
 
-function getOnePokemon(url) {
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data)
-        })
-        .catch(function(error) {
-            console.error(error)
-        });
-}
+// function getOnePokemon(url) {
+//     fetch(url)
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(data) {
+//             console.log(data)
+//         })
+//         .catch(function(error) {
+//             console.error(error)
+//         });
+// }
 
-fetch(apiUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
+// fetch(apiUrl)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
 
-        for (var i = 0; i < data.pokemon.length; i++) {
-            var pokemon = data.pokemon[i].pokemon.name;
-            var pokemonList = document.createElement('li');
-            pokemonList.innerHTML = pokemon;
-            testHeading.appendChild(pokemonList);
-        }
+//         for (var i = 0; i < data.pokemon.length; i++) {
+//             var pokemon = data.pokemon[i].pokemon.name;
+//             var pokemonList = document.createElement('li');
+//             pokemonList.innerHTML = pokemon;
+//             testHeading.appendChild(pokemonList);
+//         }
 
-        getOnePokemon(data.pokemon[0].pokemon.url)
-    })
-    .catch(function(error) {
-        console.error(error);
-    });
+//         getOnePokemon(data.pokemon[0].pokemon.url)
+//     })
+//     .catch(function(error) {
+//         console.error(error);
+//     });
